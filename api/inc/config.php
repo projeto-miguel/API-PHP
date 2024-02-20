42b4cb25001718d0a8186f30dc36afd750b4f41d
Miguel Carneiro de Mendonça <100208432+projeto-miguel@users.noreply.github.com>
2024-02-20 09:44:09 -0300

    Delete api directory

--- api/inc/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// DB ACCESS CONFIG ===================================
-define('DB_SERVER',  'localhost');
-define('DB_NAME',  'loja_api');
-define('DB_CHARSET',  'utf8mb4');
-define('DB_USERNAME',  'user_loja_api');
-define('DB_PASSWORD',  'Or/!vBw]LrSyrXX@');
